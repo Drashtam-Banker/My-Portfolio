b48e9d8de62aa0a3ff3fcc4b81fa91de50a7b037
Gautam Arora <garora38@my.centennialcollege.ca>
2025-03-05 22:46:33 -0500

    Backend

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ Drashtam Kinnar Banker
 
 */
 import React from 'react';
-import About from './components/About/about';
-import Contact from './components/Contact/contact';
-import Footer from './components/Footer/footer';
-import Intro from './components/Intro/intro';
-import Navbar from "./components/NavBar/navbar";
-import Skills from './components/Skills/skills';
-import Works from './components/Works/works';
+import About from './components/about/about.js';
+import Contact from './components/Contact/contact.js';
+import Footer from './components/Footer/footer.js';
+import Intro from './components/Intro/intro.js';
+import Navbar from "./components/NavBar/navbar.js";
+import Skills from './components/Skills/skills.js';
+import Works from './components/Works/works.js';
 
 function App() {
   return (
